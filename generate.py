1a08167b8bebf1413e4f5c15ba2246c2e5460c09
Bram de Jong <bram.dejong@gmail.com>
2018-12-29 21:25:55 +0100

    added factmag parsing and more requirements...

--- generate.py
+++ generate.py
@@ -8,9 +8,49 @@ from datetime import timedelta
 import pickle
 import local_data
 import urllib
+import json
+import lxml.html
+from cssselect import GenericTranslator, SelectorError
 
+def print_structure(data):
+    print(json.dumps(data, indent=4))
 
-class Auth:
+def chunks(items, chunk_size):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(items), chunk_size):
+        yield items[i:i + chunk_size]
+
+def get_ambient():
+    expression = GenericTranslator().css_to_xpath('div.entry-content > hr + p + p strong')
+    expression2 = GenericTranslator().css_to_xpath('strong a')
+    content = requests.get("https://www.factmag.com/2018/12/16/best-ambient-2018/").text
+    xml_tree = lxml.html.fromstring(content)
+    albums = []
+    
+    for element in xml_tree.xpath(expression):
+        if element.xpath(expression2):
+            text_content = element.text_content()
+            if text_content.startswith("Read next"):
+                continue
+            artist = text_content.split("\n")[0]
+            album = element.xpath(expression2)[0].text_content()
+            albums.append((artist, album))
+    
+    return albums
+    
+def get_best_albums():
+    expression1 = GenericTranslator().css_to_xpath(".fr_list_heading.fr-text p")
+    expression2 = GenericTranslator().css_to_xpath(".fr_list_sub_heading.fr-text p")
+    content = requests.get("https://www.factmag.com/2018/12/13/the-50-best-albums-of-2018/").text
+    xml_tree = lxml.html.fromstring(content)
+    artists = [element.text_content() for element in xml_tree.xpath(expression1)]
+    albums = [element.text_content() for element in xml_tree.xpath(expression2)]
+    return zip(artists, albums)
+
+class SpotifyException(Exception):
+    pass
+
+class Spotify:
     REDIRECT_URI = local_data.REDIRECT_URI
     CLIENT_ID = local_data.CLIENT_ID
     CLIENT_SECRET = local_data.CLIENT_SECRET
@@ -54,6 +94,10 @@ class Auth:
         with open(filename, "wb") as file:
             pickle.dump(data, file)
 
+    def _raise_if_error(self, data, message="Spotify excepton"):
+        if "error" in data:
+            raise SpotifyException(message + ": " + data["error"]["message"])
+
     def refresh_if_needed(self):
         if self.timeout > datetime.now():
             return
@@ -66,8 +110,9 @@ class Auth:
             api_url, data=data, auth=HTTPBasicAuth(self.CLIENT_ID, self.CLIENT_SECRET)
         ).json()
 
+        self._raise_if_error(tokens)
+
         self.access = tokens["access_token"]
-        self.refresh = tokens["refresh_token"]
         self.timeout = datetime.now() + timedelta(seconds=(tokens["expires_in"] - 60))
 
         self.save()
@@ -102,6 +147,8 @@ class Auth:
             api_url, data=data, auth=HTTPBasicAuth(self.CLIENT_ID, self.CLIENT_SECRET)
         ).json()
 
+        self._raise_if_error(tokens)
+
         self.access = tokens["access_token"]
         self.refresh = tokens["refresh_token"]
         self.timeout = datetime.now() + timedelta(seconds=(tokens["expires_in"] - 60))
@@ -119,11 +166,13 @@ class Auth:
     def _get_auth_header(self):
         self.refresh_if_needed()
         return dict(Authorization="Bearer " + self.access)
-
+    
     def get_user_id(self):
         url = "https://api.spotify.com/v1/me"
 
         user_details = requests.get(url, headers=self._get_auth_header()).json()
+
+        self._raise_if_error(user_details)
 
         return user_details["id"]
 
@@ -136,14 +185,20 @@ class Auth:
             api_url, json=data, headers=self._get_auth_header()
         ).json()
 
+        self._raise_if_error(playlist_details)
+
         return playlist_details
 
     def get_playlists(self, user_id):
         api_url = "https://api.spotify.com/v1/users/{}/playlists".format(user_id)
 
-        return requests.get(
+        playlists = requests.get(
             api_url, params=dict(limit=50), headers=self._get_auth_header()
         ).json()
+
+        self._raise_if_error(playlists)
+
+        return playlists
 
     def get_or_create_playlist(self, user_id, playlist_name):
         playlists = self.get_playlists(user_id)
@@ -163,41 +218,87 @@ class Auth:
             api_url, json=data, headers=self._get_auth_header()
         ).json()
 
+        self._raise_if_error(playlist_details)
+
         return playlist_details
-    
+
     def search_album(self, artist, album):
         api_url = "https://api.spotify.com/v1/search"
 
         query = "album:{} artist:{}".format(album, artist)
         params = dict(q=query, type="album")
 
-        return requests.get(
+        results = requests.get(
             api_url, params=params, headers=self._get_auth_header()
         ).json()
+
+        self._raise_if_error(results)
+
+        return results
+
+    def get_album_details(self, album_id):
+        api_url = "https://api.spotify.com/v1/albums/{}".format(album_id)
+
+        album_details = requests.get(api_url, headers=self._get_auth_header()).json()
+
+        self._raise_if_error(album_details)
+
+        return album_details
+    
+    def add_albums_to_playlist(self, album_ids, playlist_id):
+        tracks = []
         
+        for album_id in album_ids:
+            album = self.get_album_details(album_id)
+            
+            for track in album["tracks"]["items"]:
+                tracks.append(track["uri"])
+
+        for chunked_tracks in chunks(tracks, 100): # spotify accepts 100 tracks max
+            self.insert_track(playlist_id, chunked_tracks)
+
 
 def main():
     scopes = [
         "user-read-private",
         "playlist-read-private",
         "playlist-modify-public",
-        "playlist-modify-private"
+        "playlist-modify-private",
     ]
-    auth = Auth(scopes)
+    spotify = Spotify(scopes)
 
-    user_id = auth.get_user_id()
+    user_id = spotify.get_user_id()
 
-    playlist = auth.get_or_create_playlist(user_id, "FactMag Playlist")
-    playlist_id = playlist["id"]
+    if False:
+        playlist = spotify.get_or_create_playlist(user_id, "FactMag Ambient")
+        playlist_id = playlist["id"]
 
-    # track_ids = ["spotify:track:4iV5W9uYEdYUVa79Axb7Rh"]
-    # print(auth.insert_track(playlist_id, track_ids))
+        album_ids = []
+        for (artist, album) in get_ambient():
+            albums = spotify.search_album(artist, album)
 
-    albums = auth.search_album("michael jackson", "thriller")
-    print(albums["albums"]["items"][0]["id"])
+            if albums["albums"]["items"]:
+                album_ids.append(albums["albums"]["items"][0]["id"])
+            else:
+                print("Did not find", artist, album)
+        
+        spotify.add_albums_to_playlist(album_ids, playlist_id)
+    
+    if True:
+        playlist = spotify.get_or_create_playlist(user_id, "FactMag Best Albums")
+        playlist_id = playlist["id"]
 
-    # GET https://api.spotify.com/v1/albums/{id}
-    # -> all tracks are in there...
+        album_ids = []
+        for (artist, album) in get_best_albums():
+            albums = spotify.search_album(artist, album)
+
+            if albums["albums"]["items"]:
+                album_ids.append(albums["albums"]["items"][0]["id"])
+            else:
+                print("Did not find", artist, album)
+        
+        spotify.add_albums_to_playlist(album_ids, playlist_id)
+
 
 
 if __name__ == "__main__":
